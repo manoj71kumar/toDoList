e19a8693433fd1b25a266f379e420e585842eb01
Manoj Kumar <manoj.kumar@livehopper.com>
2018-03-09 00:29:01 +0530

    add commenting for functions and html pages sections

--- js/controllers/controllers.js
+++ js/controllers/controllers.js
@@ -2,14 +2,20 @@ angular.module('appController', []).controller('appController',function($scope) 
         $scope.datalist = [];
         $scope.markAll = false;
     
+//      ADD NEW DATA IN LIST
+        
         $scope.addNewList = function() {
                 $scope.datalist.push({text:$scope.addtext, done:false});
                 $scope.addtext = '';
         };
         
+//      CHECK REMAINING DATA SIZE
+        
         $scope.isShow = function() {
             return ($scope.datalist.length != $scope.rest());
         }  
+        
+//      CHECK REMAINING LIST DATA
         
         $scope.rest = function() {
             var count = 0;
@@ -19,15 +25,19 @@ angular.module('appController', []).controller('appController',function($scope) 
                 return count;
         };
         
+//      MARK ALL LIST FOR ACTION
+        
         $scope.MarkCompleteAll = function() {
             angular.forEach($scope.datalist, function(data) {
                 data.done =$scope.markAll;
             });
         };
-
+        
         $scope.item = function() {
                 return ($scope.datalist.length - $scope.rest() > 1) ? "items" : "item";     
         };
+        
+//      DELETE LIST 
       
         $scope.empty = function() {
             var prevData = $scope.datalist;
